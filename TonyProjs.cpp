44dab9eccf0efce18441c2ea187b7d1ab744048a
oleg3790 <oleg.krysko@outlook.com>
2020-08-10 21:52:56 -0500

    My version

--- TonyProjs.cpp
+++ TonyProjs.cpp
@@ -6,67 +6,104 @@
 */
 #include<iostream>
 #include<string>
+
 using namespace std;
+
 class State
 {
-private:
-    string Name();
-    string Capital();
-    int yearFounded();
-public:
-    State() { Name = "unknown"; Capital = "unknown"; yearFounded = 0; }
-    State(string nm, string cap, int year) { Name = nm; Capital = cap; yearFounded = year; }
-    void setName(string nm) { Name = nm; }
-    void setCapital(string cap) { Capital = cap; }
-    void setYearFounded(int year) { yearFounded = year; }
-    string getName() { return Name; }
-    string getCapital() { return Capital; }
-    int getYearFounded() { return yearFounded; }
-    void printState();
+
+  private:
+    string Name;
+    string Capital;
+    int yearFounded;
+
+  public:
+    State()
+    {
+        Name = "unknown";
+        Capital = "unknown";
+        yearFounded = 0;
+    }
+
+    State(string nm, string cap, int year)
+    {
+        Name = nm;
+        Capital = cap;
+        yearFounded = year;
+    }
+
+    void setName(string nm)
+    {
+        Name = nm;
+    }
+
+    void setCapital(string cap)
+    {
+        Capital = cap;
+    }
+
+    void setYearFounded(int year)
+    {
+        yearFounded = year;
+    }
+
+    string getName()
+    {
+        return Name;
+    }
+
+    string getCapital()
+    {
+        return Capital;
+    }
+
+    int getYearFounded()
+    {
+        return yearFounded;
+    }
+
+    void printState()
+    {
+        cout << "This is information about the state of: " << Name << ", " << Capital << endl;
+        cout << "The State was founded in " << yearFounded << endl;
+    }
 };
-string State::Name()
-{
-    return string();
-}
-string State::Capital()
-{
-    return string();
-}
-int State::yearFounded()
-{
-    return 0;
-}
-void State::printState()
-{
-    cout << "This is information about the state of: " << Name << ", " << Capital << endl;
-    cout << "The State was founded in " << yearFounded << endl;
-}
+
 void main()
 {
     string stateName, capital;
     int year;
-    //get input from user
     State stateName1;
+
     cout << "Enter name of State: ";
+
+    // Get input from user
     getline(cin, stateName);
     stateName1.setName(stateName);
+
     cout << "Enter name of capital: ";
     getline(cin, capital);
     stateName1.setCapital(capital);
+
     cout << "Enter year founded: ";
     cin >> year;
     cin.ignore();
     stateName1.setYearFounded(year);
+
     //print out information
     stateName1.printState();
+
     //get input from user using contstruction with parameters
     cout << "Enter name of State: ";
     getline(cin, stateName);
+
     cout << "Enter name of capital: ";
     getline(cin, capital);
+
     cout << "Enter year founded: ";
     cin >> year;
     State stateName2(stateName, capital, year);
     stateName2.printState();
+
     system("pause");
 }
